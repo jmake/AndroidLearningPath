319c1871b5e0e5a41ca132d2118beb4507a8ebd6
jmake <zvl_2006@hotmail.com>
2026-07-13 12:45:31 +0200

    Update RUNNER.sh

--- Tools/RUNNER.sh
+++ Tools/RUNNER.sh
@@ -45,8 +45,38 @@ build_android()
     echo 
 }
 
+
+setup_cmdline_tools() 
+{
+    ## Settings > Language and Frameworks > Android SDK > SDK Tools > command lines 
+    export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin 
+    sdkmanager --version 
+
+}
+
+setup_sdk()  
+{
+    export ANDROID_HOME=$HOME/Android/Sdk
+    #export PATH=$PATH:$ANDROID_HOME/emulator
+    #export PATH=$PATH:$ANDROID_HOME/tools
+    #export PATH=$PATH:$ANDROID_HOME/tools/bin
+    export PATH=$PATH:$ANDROID_HOME/platform-tools
+    adb --version  ## Android Debug Bridge version 1.0.41
+    java --version  ## openjdk 21.0.11 
+
+
+    JAVA_VERSION=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}' | cut -d. -f1)
+
+}
+
+
 install_apk() 
 {
+    AAPT_EXE=$(find "$ANDROID_HOME/build-tools" -name aapt | sort -V | tail -1)
+    AAPT_VERSION=$(basename "$(dirname "$AAPT_EXE")")
+    echo "AAPT_VERSION:'$AAPT_VERSION'"  
+
+
     find . -name "app*.apk"
 
 ##adb disconnect adb-R9YT61DRRYB-5PQ46V._adb-tls-connect._tcp
@@ -87,22 +117,71 @@ install_apk()
 }
 
 
+create_project_java_simplest() 
+{
 
-export ANDROID_HOME=$HOME/Android/Sdk
-#export PATH=$PATH:$ANDROID_HOME/emulator
-#export PATH=$PATH:$ANDROID_HOME/tools
-#export PATH=$PATH:$ANDROID_HOME/tools/bin
-export PATH=$PATH:$ANDROID_HOME/platform-tools
+    AAPT_EXE=$(find "$ANDROID_HOME/build-tools" -name aapt | sort -V | tail -1)
+    AAPT_VERSION=$(basename "$(dirname "$AAPT_EXE")")
+    echo "AAPT_VERSION:'$AAPT_VERSION'"  
 
+    JAVA_VERSION=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}' | cut -d. -f1) 
+
+    rm -rf .git* .gradle *
+    #find . -name build -type d -prune -exec rm -rf {} +
+
+    APP_NAME="app_a" 
+
+    ## groovy | kotlin 
+    printf "1\nno\n" | gradle init \
+    --dsl groovy \
+    --type java-application \
+    --test-framework junit \
+    --project-name $APP_NAME \
+    --java-version "$JAVA_VERSION"  \
+    --package "spicytech" 
+
+    rm -rf app/src/test 
+    find . #app/src/
+
+    ##ls -la 
+
+    ##./gradlew tasks
+
+    #./gradlew build
+    #./gradlew run
+    #./gradlew test
+
+
+}
+
+
+create_project_android_simplest() 
+{
+    rm gradlew*
+    rm -rf .gradle gradle 
+    rm -rf build 
+    rm -rf app/build 
+
+    ##git init 
+    gradle wrapper
+    #./gradlew tasks 
+    ./gradlew assembleDebug
+    ##./gradlew uninstallRelease 
+
+    #ls -la 
+}
+
+
+setup_sdk 
 setup_gradle
-adb --version  ## Android Debug Bridge version 1.0.41
-java --version  ## openjdk 21.0.11 
+setup_cmdline_tools
 
 ##rm -rf ~/.gradle/caches
-
-#setup_gradle
 
 ##key_create 
 #build_android
 #install_apk 
+
+
+create_project_android_simplest 
 
