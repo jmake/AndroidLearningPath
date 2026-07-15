fcf4c83c479ee9e66a4f1a228dd7479a05a0d261
jmake <zvl_2006@hotmail.com>
2026-07-15 05:42:41 -0400

    Update MainActivity.java

--- Sources/app/src/main/java/spicy/tech/MainActivity.java
+++ Sources/app/src/main/java/spicy/tech/MainActivity.java
@@ -3,34 +3,38 @@ package spicy.tech;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 
 import com.polar.sdk.api.errors.PolarInvalidArgument;
 
 public class MainActivity extends Activity
 {
-    private FileLogger logger;
-    private PolarConnection polarConnection;
+    private FileLogger fileLogger = null;
+    private PolarConnection polarConnection = null;
     private static final int CREATE_FILE_REQUEST = 100;
 
     private int row = 0;
-    private long time0;
-    private Thread loggingThread;
+    private long time0 = 0;
+    private Thread loggingThread = null;
     private volatile boolean logging = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView( R.layout.activity_main );
 
-        logger = new FileLogger(this);
+        String msg = "New text";
+        TextView textView = findViewById( R.id.textView );
+        textView.setText( msg );
+
         polarConnection = new PolarConnection(this);
         polarConnection.onCreate();
 
-        startActivityForResult(
-                logger.createFilePickerIntent(),
-                CREATE_FILE_REQUEST
-        );
+        fileLogger = new FileLogger(this);
+        startActivityForResult(fileLogger.createFilePickerIntent(), CREATE_FILE_REQUEST);
     }
 
     @Override
@@ -38,54 +42,74 @@ public class MainActivity extends Activity
     {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == CREATE_FILE_REQUEST && resultCode == RESULT_OK && data != null) {
-            logger.setFileUri(data.getData());
-            time0 = System.currentTimeMillis();
-            logger.append("time;value1;value2;value3");
-            startLogging();
+        if (fileLogger != null)
+        {
+            if (requestCode == CREATE_FILE_REQUEST && resultCode == RESULT_OK && data != null)
+            {
+                time0 = System.currentTimeMillis();
+                fileLogger.setFileUri(data.getData());
+                fileLogger.append("time;value1;value2;value3");
+                startLogging();
+            }
         }
+
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+    public void onRequestPermissionsResult(
+            int requestCode,
+            @NonNull String[] permissions,
+            @NonNull int[] grantResults
+    )
+    {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        polarConnection.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (polarConnection != null) polarConnection.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     private void startLogging()
     {
+        if (fileLogger == null) return ;
+
         logging = true;
-        loggingThread = new Thread(() -> {
+        loggingThread = new Thread(() ->
+        {
             while (logging) {
                 long time = System.currentTimeMillis();
                 long value1 = time - time0;
                 int value2 = row;
                 double value3 = Math.sin(2.0 * Math.PI * value1 / 1000.0);
 
-                logger.append(time + ";" + value1 + ";" + value2 + ";" + value3);
+                fileLogger.append(time + ";" + value1 + ";" + value2 + ";" + value3);
                 row++;
-
+                /*
                 try {
                     Thread.sleep(10);
                 } catch (InterruptedException e) {
                     return;
                 }
+                */
             }
         });
         loggingThread.start();
     }
 
     @Override
-    protected void onDestroy() {
+    protected void onDestroy()
+    {
         super.onDestroy();
+
         logging = false;
-        if (loggingThread != null) {
-            loggingThread.interrupt();
+        if (loggingThread != null) loggingThread.interrupt();
+
+        try
+        {
+            if (polarConnection != null) polarConnection.onDestroy();
         }
-        try {
-            polarConnection.onDestroy();
-        } catch (PolarInvalidArgument e) {
+        catch (PolarInvalidArgument e)
+        {
             throw new RuntimeException(e);
         }
+
     }
 }
