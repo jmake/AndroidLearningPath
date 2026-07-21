95da357ac46f5819572aabfa24539182302dab03
jmake <zvl_2006@hotmail.com>
2026-07-21 08:08:16 -0400

    Update GraphSurfaceView.java

--- Sources/app/src/main/java/spicy/tech/GraphSurfaceView.java
+++ Sources/app/src/main/java/spicy/tech/GraphSurfaceView.java
@@ -1,17 +1,17 @@
 package spicy.tech;
 
-
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.DashPathEffect;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-public class GraphSurfaceView extends SurfaceView implements SurfaceHolder.Callback
-{
+public class GraphSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+
     private DrawThread thread;
 
     public GraphSurfaceView(Context context) {
@@ -61,6 +61,7 @@ public class GraphSurfaceView extends SurfaceView implements SurfaceHolder.Callb
         }
     }
 
+
     private static class DrawThread extends Thread {
 
         private final SurfaceHolder holder;
@@ -68,20 +69,34 @@ public class GraphSurfaceView extends SurfaceView implements SurfaceHolder.Callb
         private boolean running = true;
         private float t = 0;
 
-        private final Paint paint;
-        private final Path path;
+        private final Paint signalPaint;
+        private final Paint axisPaint;
+        private final Paint gridPaint;
+        private final Paint markerPaint;
+
+        private final Path signalPath;
+
+        private static final float Y_MIN = -1.2f;
+        private static final float Y_MAX = 1.2f;
+
 
         DrawThread(SurfaceHolder holder) {
+
             this.holder = holder;
 
-            paint = new Paint();
-            paint.setAntiAlias(true);
-            paint.setColor(Color.BLUE);
-            paint.setStyle(Paint.Style.STROKE);
-            paint.setStrokeWidth(4);
+            signalPaint = createPaint(Color.BLUE, 4, Paint.Style.STROKE);
+            axisPaint = createPaint(Color.BLACK, 2, Paint.Style.STROKE);
 
-            path = new Path();
+            gridPaint = createPaint(Color.GRAY, 1, Paint.Style.STROKE);
+            gridPaint.setPathEffect(
+                    new DashPathEffect(new float[]{10, 10}, 0)
+            );
+
+            markerPaint = createPaint(Color.RED, 2, Paint.Style.STROKE);
+
+            signalPath = new Path();
         }
+
 
         @Override
         public void run() {
@@ -91,35 +106,12 @@ public class GraphSurfaceView extends SurfaceView implements SurfaceHolder.Callb
                 Canvas canvas = null;
 
                 try {
+
                     canvas = holder.lockCanvas();
 
                     if (canvas != null) {
 
-                        canvas.drawColor(Color.WHITE);
-
-                        float width = canvas.getWidth();
-                        float height = canvas.getHeight();
-
-                        path.reset();
-
-                        for (int x = 0; x < width; x++) {
-
-                            float time = t + x * 0.02f;
-
-                            float y = (float) Math.sin(time);
-
-                            float screenY =
-                                    height / 2f -
-                                            y * height / 3f;
-
-                            if (x == 0) {
-                                path.moveTo(x, screenY);
-                            } else {
-                                path.lineTo(x, screenY);
-                            }
-                        }
-
-                        canvas.drawPath(path, paint);
+                        drawFrame(canvas);
 
                         t += 0.05f;
                     }
@@ -131,12 +123,141 @@ public class GraphSurfaceView extends SurfaceView implements SurfaceHolder.Callb
                     }
                 }
 
+
                 try {
                     sleep(16);
                 } catch (InterruptedException ignored) {
                 }
             }
         }
+
+
+        private void drawFrame(Canvas canvas) {
+
+            canvas.drawColor(Color.WHITE);
+
+            float width = canvas.getWidth();
+            float height = canvas.getHeight();
+
+            drawGrid(canvas, width, height);
+            drawAxis(canvas, width, height);
+            drawSignal(canvas, width, height);
+            drawCurrentMarker(canvas, width, height);
+        }
+
+
+        private void drawGrid(Canvas canvas, float width, float height) {
+
+            float zeroY = mapY(0, height);
+
+            for (float y = Y_MIN; y <= Y_MAX; y += 0.2f) {
+
+                float screenY = mapY(y, height);
+
+                canvas.drawLine(
+                        0,
+                        screenY,
+                        width,
+                        screenY,
+                        gridPaint
+                );
+            }
+        }
+
+
+        private void drawAxis(Canvas canvas, float width, float height) {
+
+            float zeroY = mapY(0, height);
+
+            canvas.drawLine(
+                    0,
+                    zeroY,
+                    width,
+                    zeroY,
+                    axisPaint
+            );
+
+            canvas.drawLine(
+                    0,
+                    0,
+                    0,
+                    height,
+                    axisPaint
+            );
+        }
+
+
+        private void drawSignal(Canvas canvas, float width, float height) {
+
+            signalPath.reset();
+
+            for (int x = 0; x < width; x++) {
+
+                float time = t + x * 0.02f;
+
+                float y = (float) Math.sin(time);
+
+                float screenY = mapY(y, height);
+
+                if (x == 0) {
+                    signalPath.moveTo(x, screenY);
+                } else {
+                    signalPath.lineTo(x, screenY);
+                }
+            }
+
+            canvas.drawPath(signalPath, signalPaint);
+        }
+
+
+        private void drawCurrentMarker(
+                Canvas canvas,
+                float width,
+                float height) {
+
+            float lastTime = t + width * 0.02f;
+
+            float lastY = (float) Math.sin(lastTime);
+
+            float screenY = mapY(lastY, height);
+
+            canvas.drawLine(
+                    width - 1,
+                    0,
+                    width - 1,
+                    height,
+                    markerPaint
+            );
+
+            canvas.drawCircle(
+                    width - 1,
+                    screenY,
+                    6,
+                    markerPaint
+            );
+        }
+
+
+        private float mapY(float value, float height) {
+
+            return height -
+                    ((value - Y_MIN) / (Y_MAX - Y_MIN)) * height;
+        }
+
+
+        private Paint createPaint(
+                int color,
+                float width,
+                Paint.Style style) {
+
+            Paint paint = new Paint();
+
+            paint.setAntiAlias(true);
+            paint.setColor(color);
+            paint.setStrokeWidth(width);
+            paint.setStyle(style);
+
+            return paint;
+        }
     }
 }
-
